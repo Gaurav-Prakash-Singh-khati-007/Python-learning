01fbe25043e503871fe2adde36afcbcdbec14d10
Gaurav-Prakash-Singh-khati-007 <gkhati275@gmail.com>
2024-07-01 08:03:10 -0700

    Setup Done

---
+++ first.py
@@ -0,0 +1,19 @@
+import keyword #This import all the keywords exist in python  there are total 35
+print(keyword.softkwlist) #This show all keywords which are muttable 
+print("                                                                                 ")
+print(keyword.kwlist) #this print all the keywords muttable and non muttable 
+
+
+
+
+# Identifiers   
+    #Variable is the name given to a value for easy calling 
+
+    # Its a phenomena of Assigning A name to a variable
+    #-------------------Rule--------------------------#
+        #--1. Can't Be a Keywords
+        #--2. Can Be alphaNumeric but can't start with a Number 
+        #--3. Can't contain Space in between Variable .
+        #--4. Special charchter not acceptable except  ('_') 
+
+
