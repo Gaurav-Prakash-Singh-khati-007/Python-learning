d683531e5ae85543342d251c28223347c3553cb6
Gaurav-Prakash-Singh-khati-007 <gkhati275@gmail.com>
2024-07-01 16:23:16 -0700

    List learned

--- Slicing.py
+++ Slicing.py
@@ -1,4 +1,4 @@
-# Slicing is the phenomena of extracting subdata, substring from the given data 
+# Slicing is the phenomena of extracting subdata, substring from the given string
 
 a="Cat is Black"
 print(a[0:3])
@@ -6,3 +6,8 @@ print(a[3:6])
 print(a[4:9])
 print(a[1:6])
 print(a[2:11+1:3])
+
+
+# Negative slicing 
+
+print(a[-1:-6:-1])
---
+++ list.py
@@ -0,0 +1,2 @@
+a=[1,9.8,'hello',4+5j]
+print(type(a))
